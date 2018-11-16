3ae814411d0a39d3acf27c32106960662538a7a0
Иван Шумилин <shuma-98@hotmail.com>
2018-11-16 20:58:05 +0300

    Update task 3

---
+++ Task 3/BibarySearch.Tests/BinarySearchTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchDemo
+{
+    [TestFixture]
+    public class BinarySearchTests
+    {
+        BinarySearch bs = new BinarySearch();
+
+        [TestCase(new int[] { 10, 22, 34, 48, 51, 62, 79, 88, 976 }, 62, ExpectedResult = 5)]
+        [TestCase(new int[] { 10, 22, 34, 48, 51, 62, 79, 88, 976 }, 10, ExpectedResult = 0)]
+        [TestCase(new int[] { 10, 22, 34, 48, 51, 62, 79, 88, 976 }, 976, ExpectedResult = 8)]
+        public int BinarySearch_ValidIntFormat_ValidResult(int[] arr, int target)
+        {
+            List<int> list = arr.ToList();
+            return bs.Search(list, target, Comparer<int>.Default);
+        }
+
+        [TestCase(new string[] { "10", "22", "34", "48", "51", "62", "79", "88", "976" }, "62", ExpectedResult = 5)]
+        [TestCase(new string[] { "10", "22", "34", "48", "51", "62", "79", "88", "976" }, "10", ExpectedResult = 0)]
+        [TestCase(new string[] { "10", "22", "34", "48", "51", "62", "79", "88", "976" }, "976", ExpectedResult = 8)]
+        public int BinarySearch_ValidStringFormat_ValidResult(string[] arr, string target)
+        {
+            List<string> list = arr.ToList();
+            return bs.Search(list, target, Comparer<string>.Default);
+        }
+
+        [TestCase(new string[] { "10", "22", "34", "48", "51", "62", "79", "88", "976" }, "976")]
+        public void BinarySearch_InvalidComparerFormat_ArgumentNullException(string[] arr, string target)
+        {
+            List<string> list = arr.ToList();
+            Assert.Throws<ArgumentNullException>(() => bs.Search(list, target, null));
+        }
+    }
+}
---
+++ Task 3/BibarySearch/BinarySearch.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchDemo
+{
+    public class BinarySearch
+    {
+        #region private methods
+        private bool IsEmpty<T>(List<T> list)
+        {
+            if (list.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsSort<T>(List<T> list, IComparer<T> comparer)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (comparer.Compare(list[i - 1], list[i]) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        #endregion
+
+        #region public methods
+        public int Search<T>(List<T> list, T target, IComparer<T> comparer)
+        {
+            int first = 0;
+            int last = list.Count;
+            int mid;
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (IsEmpty(list) || !IsSort(list, comparer))
+            {
+                throw new Exception("Bad data format!");
+            }
+
+            while (first <= last)
+            {
+                mid = (first + last) / 2;
+
+                if (comparer.Compare(list[mid], target) == 0)
+                {
+                    return mid;
+                }
+                else if (comparer.Compare(list[mid], target) < 0)
+                {
+                    first = mid + 1;
+                }
+                else
+                {
+                    last = mid - 1;
+                }
+            }
+
+            return -11;
+        }
+        #endregion
+    }
+
+}
